94ab2d376e3c8148c3f1bf13032ab64f6e96acf9
Libby Horacek <libby@positiondev.com>
2015-10-27 08:40:56 -0400

    Wow, totally lost track there.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -2,31 +2,31 @@
 
 module Main where
 
-import Web.Scotty
-import qualified Web.Scotty as S
+import           Web.Scotty
+import qualified Web.Scotty                           as S
 
-import Network.Wai.Middleware.RequestLogger
-import Network.Wai.Middleware.Static
-import Network.Wai.Parse
+import           Network.Wai.Middleware.RequestLogger
+import           Network.Wai.Middleware.Static
+import           Network.Wai.Parse
 
-import Text.Blaze.Html5 hiding (main, map)
-import Text.Blaze.Html5.Attributes
-import qualified Text.Blaze.Html5 as H hiding (main, map)
-import qualified Text.Blaze.Html5.Attributes as A
-import Text.Blaze.Html.Renderer.Text (renderHtml)
+import           Text.Blaze.Html.Renderer.Text        (renderHtml)
+import           Text.Blaze.Html5                     hiding (main, map)
+import qualified Text.Blaze.Html5                     as H hiding (main, map)
+import           Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5.Attributes          as A
 
 -- OMG I HATE TEXT IN HASKELL
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text as T
+import qualified Data.ByteString.Char8                as BS
+import qualified Data.ByteString.Lazy                 as B
+import qualified Data.Text                            as T
+import qualified Data.Text.Lazy                       as LT
 
-import Control.Monad.Trans.Either
-import Control.Monad.IO.Class (liftIO)
+import           Control.Monad.IO.Class               (liftIO)
+import           Control.Monad.Trans.Either
 
-import KissSet
-import Home
-import Upload
+import           Index
+import           KissSet
+import           Upload
 
 blaze = S.html . renderHtml
 
@@ -39,12 +39,11 @@ main = scotty 3000 $ do
 
   post "/upload" $ do
     fs <- files
-    relDir <- liftIO $ runEitherT $ getRelDir fs 
+    relDir <- liftIO $ runEitherT $ getRelDir fs
     cels <- liftIO $ runEitherT $ processSet fs
     -- augh nesting, no sir I don't like it
     case relDir of
-      Right dir -> case cels of 
+      Right dir -> case cels of
                      Right x -> blaze $ KissSet.render dir (reverse x)
                      Left  e -> S.text $ LT.fromStrict e
       Left e -> S.text $ LT.fromStrict e
-
