8d4a8ae06b147eca9918549867bdb9be6396c5a4
Libby Horacek <libby@daydrea.me>
2017-10-01 14:30:37 -0400

    Move tests to app directory

--- app/tests/Common.hs
+++ app/tests/Common.hs
@@ -6,6 +6,7 @@ import           Control.Lens               ((^.))
 import           Control.Monad              (void)
 import           Data.Pool                  (withResource)
 import qualified Database.PostgreSQL.Simple as PG
+import           System.Environment         (setEnv)
 import           Test.Hspec
 import           Test.Hspec.Fn
 
@@ -14,8 +15,13 @@ import           Web
 
 fnTests :: SpecWith (FnHspecState Ctxt) -> Spec
 fnTests fnSpecs = do
+  runIO $ setEnv "ENV" "test"
   ctxt <- runIO initializer
-  fn (return ctxt) appBase [] (const $ return ()) (afterEval clearTables $ fnSpecs)
+  fn (return ctxt) -- how to set up the context
+     appBase -- turn the context into a WAI application
+     [] -- list of middleware
+     (const $ return ()) -- clean up what to do after tests are over
+     (afterEval clearTables $ fnSpecs) -- run the tests!
 
 clearTables :: Ctxt -> IO ()
 clearTables ctxt =
