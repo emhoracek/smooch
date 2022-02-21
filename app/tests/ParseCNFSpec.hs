5f50a934fd372205c3c3dc44ba60e58e5a39eafd
Libby Horacek <libby@daydrea.me>
2022-02-21 15:42:17 -0500

    Don't group cels into objects on backend
    We were running into some subtle bugs relating to layering on the
    frontend and it's hard to spot where they were coming from. If we
    move some of the cel processing to the frontend, we can use a more
    imperative process that's closer to how earlier KiSS viewers worked.

--- app/tests/ParseCNFSpec.hs
+++ app/tests/ParseCNFSpec.hs
@@ -9,8 +9,8 @@ import           Text.ParserCombinators.Parsec
 import           Kiss
 import           ParseCNF
 
-fakeKissCel :: Int -> String -> Int -> [Int] -> Int -> KissCel
-fakeKissCel a s b cs d = KissCel a s b cs d (Position 0 0)
+fakeKissCel :: Int -> Int -> String -> Int -> [Int] -> Int -> KissCel
+fakeKissCel e a s b cs d = KissCel e a s b cs d (Position 0 0)
 
 sampleCel1 :: String
 sampleCel1 = "#19       markr6.cel      *0 : 0 1 2 3 4 5 6 7 8 9"
@@ -80,26 +80,27 @@ spec = do
     it "parses a CNF into KiSS data" $
       runExceptT (getKissData sampleKiss) `shouldReturn`
         Right (CNFKissData 0 0 ["colors.kcf"] (756, 398)
-          [ KissObject 1 [ fakeKissCel 0 "shirt" 0 [0,1,2,3] 0] [Position 1 1],
-            KissObject 2 [ fakeKissCel 0 "body"  0 [0,1,2,3] 0] [Position 2 2],
-            KissObject 3 [ fakeKissCel 0 "shirtb" 0 [0,1,2,3] 0] [Position 3 3]])
+          [ fakeKissCel 1 0 "shirt" 0 [0,1,2,3] 0,
+            fakeKissCel 2 0 "body"  0 [0,1,2,3] 0,
+            fakeKissCel 3 0 "shirtb" 0 [0,1,2,3] 0] [KissSetPos 0  [NoPosition, Position 1 1, Position 2 2, Position 3 3]])
     it "parses a CNF into KiSS data" $
       runExceptT (getKissData sampleKiss2) `shouldReturn`
         Right (CNFKissData 0 0 ["colors.kcf"] (756, 398)
-          [ KissObject 1 [ fakeKissCel 0 "shirt" 0 [0,1,2,3] 0,
-                           fakeKissCel 0 "shirtb" 0 [0,1,2,3] 0 ] [Position 0 0]])
+          [ fakeKissCel 1 0 "shirt" 0 [0,1,2,3] 0,
+            fakeKissCel 1 0 "shirtb" 0 [0,1,2,3] 0 ] [KissSetPos 0 [NoPosition, Position 0 0]])
     it "parses a CNF into KiSS data even with idiosyncratic caps" $
       runExceptT (getKissData sampleKiss3) `shouldReturn`
         Right (CNFKissData 0 0 ["colors.kcf"] (756, 398)
-          [ KissObject 1 [ fakeKissCel 0 "shirt" 0 [0,1,2,3] 0,
-                           fakeKissCel 0 "shirtb" 0 [0,1,2,3] 0 ] [Position 1 1],
-            KissObject 2 [ fakeKissCel 0 "body"  0 [0,1,2,3] 0] [Position 2 2] ])
+          [ fakeKissCel 1 0 "shirt" 0 [0,1,2,3] 0,
+            fakeKissCel 2 0 "body"  0 [0,1,2,3] 0,
+            fakeKissCel 1 0 "shirtb" 0 [0,1,2,3] 0 ]
+            [KissSetPos 0 [NoPosition, Position 1 1, Position 2 2]])
     it "parses a CNF into KiSS data even with odd formatting/encoding" $
       runExceptT (getKissData sampleKiss4) `shouldReturn`
         Right (CNFKissData 0 0 ["colors.kcf"] (756, 398)
-          [ KissObject 1 [ fakeKissCel 0 "shirt" 0 [0,1,2,3] 0,
-                           fakeKissCel 0 "shirtb" 0 [0..9] 0 ] [Position 1 1],
-            KissObject 2 [ fakeKissCel 0 "body"  0 [0,1,2,3] 0] [Position 2 2] ])
+          [ fakeKissCel 1 0 "shirt" 0 [0,1,2,3] 0,
+            fakeKissCel 2 0 "body"  0 [0,1,2,3] 0,
+            fakeKissCel 1 0 "shirtb" 0 [0..9] 0] [KissSetPos 0 [NoPosition, Position 1 1, Position 2 2]])
 
     it "returns an error message for a bad cnf" $
       runExceptT (getKissData "I'm not a CNF.") `shouldReturn`
@@ -108,19 +109,19 @@ spec = do
   describe "getKissCels" $
     it "parses a CNF into a list of KiSS cels" $
       runExceptT (getKissCels sampleKiss) `shouldReturn`
-         Right [ CNFKissCel 0 "shirt" 0 [0,1,2,3] 0,
-            CNFKissCel 0 "body"  0 [0,1,2,3] 0,
-            CNFKissCel 0 "shirtb" 0 [0,1,2,3] 0 ]
+         Right [ CNFKissCel 1 0 "shirt" 0 [0,1,2,3] 0,
+                 CNFKissCel 2 0 "body"  0 [0,1,2,3] 0,
+                 CNFKissCel 3 0 "shirtb" 0 [0,1,2,3] 0 ]
   describe "parseCNFLine" $ do
     it "parses a single KiSS cel" $
       parse parseCNFLine "blah" sampleCel1 `shouldBe`
-        Right (CNFCel (19, CNFKissCel 0 "markr6" 0 [0,1,2,3,4,5,6,7,8,9] 0))
+        Right (CNFCel (CNFKissCel 19 0 "markr6" 0 [0,1,2,3,4,5,6,7,8,9] 0))
     it "parses a single KiSS cel (with transparency)" $
       parse parseCelLine "blah" sampleCel2 `shouldBe`
-        Right (CNFCel (25, CNFKissCel 0 "tights1" 0 [0,1,2,3,4,5,6,7,8,9] 75))
+        Right (CNFCel (CNFKissCel 25 0 "tights1" 0 [0,1,2,3,4,5,6,7,8,9] 75))
     it "parses a single KiSS cel (different sets and fix val)" $
       parse parseCelLine "blah" sampleCel3 `shouldBe`
-        Right (CNFCel (180, CNFKissCel 99 "handl" 0 [1,4,7] 0))
+        Right (CNFCel (CNFKissCel 180 99 "handl" 0 [1,4,7] 0))
     it "parses a the positions and palette for a single set in a set" $
       parse parseCNFLine "blah" sampleSet1 `shouldBe`
         Right (CNFSetPos (KissSetPos 2
@@ -133,27 +134,3 @@ spec = do
       parse parseCNFLine "blah" sampleSet3 `shouldBe`
         Right (CNFSetPos (KissSetPos 0
                           [Position 192 11, NoPosition, Position 56 17]))
-
-    it "joins cels into objects" $
-      linesToObjects [(1, CNFKissCel 0 "shirt" 0 [0,1,2,3] 0),
-                      (2, CNFKissCel 1 "body" 0 [0,1,2,3] 0),
-                      (1, CNFKissCel 0 "shirtb" 0 [0,1,2,3] 0) ]
-                     [KissSetPos 0 [NoPosition, Position 200 200, Position 100 100],
-                      KissSetPos 0 [NoPosition, Position 100 100, Position 100 100 ]] `shouldBe`
-      [ KissObject 1 [ fakeKissCel 0 "shirt" 0 [0,1,2,3] 0,
-                       fakeKissCel 0 "shirtb" 0 [0,1,2,3] 0 ]
-        [Position 200 200, Position 100 100],
-        KissObject 2 [ fakeKissCel 1 "body" 0 [0,1,2,3] 0]
-        [Position 100 100, Position 100 100] ]
-
-    it "combines cels and adds positions to objs" $
-      combineCelsAndPositions
-        [ (1, CNFKissCel 0 "shirt" 0 [0,1,2,3] 0),
-          (1, CNFKissCel 0 "shirtb" 0 [0,1,2,3] 0),
-          (2, CNFKissCel 1 "body" 0 [0,1,2,3] 0)]
-        [(0, [NoPosition]),
-         (1, [Position 200 200]),
-         (2, [Position 100 100])] `shouldBe`
-        [(1, [CNFKissCel 0 "shirt" 0 [0,1,2,3] 0,
-              CNFKissCel 0 "shirtb" 0 [0,1,2,3] 0], [Position 200 200]),
-         (2, [ CNFKissCel 1 "body" 0 [0,1,2,3] 0], [Position 100 100])]
